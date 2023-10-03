0603e06e65a98a9e3c526e81e2d533f675a86045
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-03 11:59:34 -0300

    add state for display

--- src/components/DrumMachine/DrumMachine.jsx
+++ src/components/DrumMachine/DrumMachine.jsx
@@ -5,11 +5,16 @@ import styles from './DrumMachine.module.css'
 
 const DrumMachine = () => {
   const [soundGroup, setSoundGroup] = useState('first')
+  const [display, setDisplay] = useState('No play yet')
+
+  const handleSetDisplay = (newDisplay) => {
+    setDisplay(newDisplay)
+  }
 
   return (
     <main id="drum-machine" className={styles.drumMachine}>
-      <DrumPad soundGroup={soundGroup}/>
-      <LateralPanel />
+      <DrumPad soundGroup={soundGroup} handleSetDisplay={handleSetDisplay} />
+      <LateralPanel display={display}/>
     </main>
   )
 }
