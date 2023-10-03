d1e5552363c6cee979138351e66ed3fcf674a8ef
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-03 12:05:50 -0300

    add keydown functionality to play audio

--- src/components/DrumPad/components/Pad/Pad.jsx
+++ src/components/DrumPad/components/Pad/Pad.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { playAudio } from '../../../../utils/helper/playAudio'
 
 const Pad = ({ sound, handleSetDisplay }) => {
+  const handleKeyDown = (event) => {
+    if (event.keyCode === sound.keyCode) {
+      playAudio(sound, handleSetDisplay)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown)
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [sound])
+
   return (
-    <div className="drum-pad" id={sound.id} onClick={() => playAudio(sound, handleSetDisplay)}>
+    <div
+      className="drum-pad"
+      id={sound.id}
+      onClick={() => playAudio(sound, handleSetDisplay)}
+    >
       {sound.key}
       <audio className="clip" id={sound.key} src={sound.url} />
     </div>
