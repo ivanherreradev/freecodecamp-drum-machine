cc1d88a0247f91c5367846d24ea0b3f1d0202510
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-03 12:34:03 -0300

    add styles (LateralPanel)

--- src/components/LateralPanel/LateralPanel.jsx
+++ src/components/LateralPanel/LateralPanel.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Display from './components/Display/Display'
 import SoundGroupSelector from './components/SoundGroupSelector/SoundGroupSelector'
+import styles from './LateralPanel.module.css'
 
 const LateralPanel = ({ display, handleSetSoundGroup, soundGroup }) => {
   return (
-    <aside>
+    <aside className={styles.lateralPanel}>
       <Display currentMessage={display} />
       <SoundGroupSelector
         handleSetSoundGroup={handleSetSoundGroup}
