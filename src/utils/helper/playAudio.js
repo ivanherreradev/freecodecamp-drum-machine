e5453874bf6cceadee9a58fc973671c795ebc3a9
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-03 17:02:00 -0300

    add volume to play

--- src/utils/helper/playAudio.js
+++ src/utils/helper/playAudio.js
@@ -1,6 +1,7 @@
-export const playAudio = (sound, handleSetDisplay) => {
+export const playAudio = (sound, volume, handleSetDisplay) => {
   handleSetDisplay(sound.id)
   const audio = document.getElementById(sound.key)
+  audio.volume = volume
   audio.currentTime = 0
   audio.play()
 }
