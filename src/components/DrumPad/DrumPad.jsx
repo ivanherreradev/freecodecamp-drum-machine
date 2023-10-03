c9f45ed36971d459539803c201d88190ca648a10
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-03 12:07:42 -0300

    add styles (DrumPad)

--- src/components/DrumPad/DrumPad.jsx
+++ src/components/DrumPad/DrumPad.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { firstSoundsGroup, secondSoundsGroup } from '../../utils/sounds/sounds'
 import Pad from './components/Pad/Pad'
+import { firstSoundsGroup, secondSoundsGroup } from '../../utils/sounds/sounds'
+import styles from './DrumPad.module.css'
 
 const DrumPad = ({ soundsGroup, handleSetDisplay }) => {
   const currentSoundGroup =
     soundsGroup === 'first' ? firstSoundsGroup : secondSoundsGroup
 
   return (
-    <section id="drum-pads">
+    <section id="drum-pads" className={styles.drumPad}>
       {currentSoundGroup.map((sound) => (
-        <Pad key={sound.key} sound={sound} handleSetDisplay={handleSetDisplay}/>
+        <Pad
+          key={sound.key}
+          sound={sound}
+          handleSetDisplay={handleSetDisplay}
+        />
       ))}
     </section>
   )
