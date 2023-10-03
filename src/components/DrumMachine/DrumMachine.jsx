96ffc046c94811d55353331ccbad0d2d61f30ddd
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-03 17:23:24 -0300

    add state and function to mute app

--- src/components/DrumMachine/DrumMachine.jsx
+++ src/components/DrumMachine/DrumMachine.jsx
@@ -7,6 +7,7 @@ const DrumMachine = () => {
   const [soundGroup, setSoundGroup] = useState('first')
   const [display, setDisplay] = useState('No play yet')
   const [volume, setVolume] = useState(0.5)
+  const [isMuted, setIsMuted] = useState(false)
 
   const handleSetDisplay = (newDisplay) => {
     setDisplay(newDisplay)
@@ -20,19 +21,26 @@ const DrumMachine = () => {
     setVolume(newVolume)
   }
 
+  const handleSetMute = () => {
+    isMuted ? setIsMuted(false) : setIsMuted(true)
+  }
+
   return (
     <main id="drum-machine" className={styles.drumMachine}>
       <DrumPad
         soundGroup={soundGroup}
         volume={volume}
+        isMuted={isMuted}
         handleSetDisplay={handleSetDisplay}
       />
       <LateralPanel
         display={display}
         soundGroup={soundGroup}
         volume={volume}
+        isMuted={isMuted}
         handleSetSoundGroup={handleSetSoundGroup}
         handleSetVolume={handleSetVolume}
+        handleSetMute={handleSetMute}
       />
     </main>
   )
