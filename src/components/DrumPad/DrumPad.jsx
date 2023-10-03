6495e16564d84f1af9cb8e2f61daea486de914b2
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-03 12:00:42 -0300

    add function to set display

--- src/components/DrumPad/DrumPad.jsx
+++ src/components/DrumPad/DrumPad.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { firstSoundsGroup, secondSoundsGroup } from '../../utils/sounds/sounds'
 import Pad from './components/Pad/Pad'
 
-const DrumPad = ({ soundsGroup }) => {
+const DrumPad = ({ soundsGroup, handleSetDisplay }) => {
   const currentSoundGroup =
     soundsGroup === 'first' ? firstSoundsGroup : secondSoundsGroup
 
   return (
     <section id="drum-pads">
       {currentSoundGroup.map((sound) => (
-        <Pad key={sound.key} sound={sound} />
+        <Pad key={sound.key} sound={sound} handleSetDisplay={handleSetDisplay}/>
       ))}
     </section>
   )
--- src/components/DrumPad/components/Pad/Pad.jsx
+++ src/components/DrumPad/components/Pad/Pad.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { playAudio } from '../../../../utils/helper/playAudio'
 
-const Pad = ({ sound }) => {
+const Pad = ({ sound, handleSetDisplay }) => {
   return (
-    <div className="drum-pad" id={sound.id}>
+    <div className="drum-pad" id={sound.id} onClick={() => playAudio(sound, handleSetDisplay)}>
       {sound.key}
       <audio className="clip" id={sound.key} src={sound.url} />
     </div>
